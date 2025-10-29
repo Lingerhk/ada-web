02388c62b7de79d05eae850cb83446c28d6c78fa
Lingerhk <lingerhk@gmail.com>
2025-10-29 09:53:20 +0000

    remove rpc GetPwdUpdateTm

--- src/api/grpc/ada.ts
+++ src/api/grpc/ada.ts
@@ -547,28 +547,6 @@ export interface ResetPasswordReply {
     result: string;
 }
 /**
- * @generated from protobuf message ada.GetPwdUpdateTmReq
- */
-export interface GetPwdUpdateTmReq {
-    /**
-     * @generated from protobuf field: string userName = 1;
-     */
-    userName: string;
-}
-/**
- * @generated from protobuf message ada.GetPwdUpdateTmReply
- */
-export interface GetPwdUpdateTmReply {
-    /**
-     * @generated from protobuf field: bool needChangePwd = 1;
-     */
-    needChangePwd: boolean;
-    /**
-     * @generated from protobuf field: string pwdUpdateTm = 2;
-     */
-    pwdUpdateTm: string;
-}
-/**
  * Domain
  *
  * @generated from protobuf message ada.ListDomainReq
@@ -7039,108 +7017,6 @@ class ResetPasswordReply$Type extends MessageType<ResetPasswordReply> {
  * @generated MessageType for protobuf message ada.ResetPasswordReply
  */
 export const ResetPasswordReply = new ResetPasswordReply$Type();
-// @generated message type with reflection information, may provide speed optimized methods
-class GetPwdUpdateTmReq$Type extends MessageType<GetPwdUpdateTmReq> {
-    constructor() {
-        super("ada.GetPwdUpdateTmReq", [
-            { no: 1, name: "userName", kind: "scalar", T: 9 /*ScalarType.STRING*/ }
-        ]);
-    }
-    create(value?: PartialMessage<GetPwdUpdateTmReq>): GetPwdUpdateTmReq {
-        const message = globalThis.Object.create((this.messagePrototype!));
-        message.userName = "";
-        if (value !== undefined)
-            reflectionMergePartial<GetPwdUpdateTmReq>(this, message, value);
-        return message;
-    }
-    internalBinaryRead(reader: IBinaryReader, length: number, options: BinaryReadOptions, target?: GetPwdUpdateTmReq): GetPwdUpdateTmReq {
-        let message = target ?? this.create(), end = reader.pos + length;
-        while (reader.pos < end) {
-            let [fieldNo, wireType] = reader.tag();
-            switch (fieldNo) {
-                case /* string userName */ 1:
-                    message.userName = reader.string();
-                    break;
-                default:
-                    let u = options.readUnknownField;
-                    if (u === "throw")
-                        throw new globalThis.Error(`Unknown field ${fieldNo} (wire type ${wireType}) for ${this.typeName}`);
-                    let d = reader.skip(wireType);
-                    if (u !== false)
-                        (u === true ? UnknownFieldHandler.onRead : u)(this.typeName, message, fieldNo, wireType, d);
-            }
-        }
-        return message;
-    }
-    internalBinaryWrite(message: GetPwdUpdateTmReq, writer: IBinaryWriter, options: BinaryWriteOptions): IBinaryWriter {
-        /* string userName = 1; */
-        if (message.userName !== "")
-            writer.tag(1, WireType.LengthDelimited).string(message.userName);
-        let u = options.writeUnknownFields;
-        if (u !== false)
-            (u == true ? UnknownFieldHandler.onWrite : u)(this.typeName, message, writer);
-        return writer;
-    }
-}
-/**
- * @generated MessageType for protobuf message ada.GetPwdUpdateTmReq
- */
-export const GetPwdUpdateTmReq = new GetPwdUpdateTmReq$Type();
-// @generated message type with reflection information, may provide speed optimized methods
-class GetPwdUpdateTmReply$Type extends MessageType<GetPwdUpdateTmReply> {
-    constructor() {
-        super("ada.GetPwdUpdateTmReply", [
-            { no: 1, name: "needChangePwd", kind: "scalar", T: 8 /*ScalarType.BOOL*/ },
-            { no: 2, name: "pwdUpdateTm", kind: "scalar", T: 9 /*ScalarType.STRING*/ }
-        ]);
-    }
-    create(value?: PartialMessage<GetPwdUpdateTmReply>): GetPwdUpdateTmReply {
-        const message = globalThis.Object.create((this.messagePrototype!));
-        message.needChangePwd = false;
-        message.pwdUpdateTm = "";
-        if (value !== undefined)
-            reflectionMergePartial<GetPwdUpdateTmReply>(this, message, value);
-        return message;
-    }
-    internalBinaryRead(reader: IBinaryReader, length: number, options: BinaryReadOptions, target?: GetPwdUpdateTmReply): GetPwdUpdateTmReply {
-        let message = target ?? this.create(), end = reader.pos + length;
-        while (reader.pos < end) {
-            let [fieldNo, wireType] = reader.tag();
-            switch (fieldNo) {
-                case /* bool needChangePwd */ 1:
-                    message.needChangePwd = reader.bool();
-                    break;
-                case /* string pwdUpdateTm */ 2:
-                    message.pwdUpdateTm = reader.string();
-                    break;
-                default:
-                    let u = options.readUnknownField;
-                    if (u === "throw")
-                        throw new globalThis.Error(`Unknown field ${fieldNo} (wire type ${wireType}) for ${this.typeName}`);
-                    let d = reader.skip(wireType);
-                    if (u !== false)
-                        (u === true ? UnknownFieldHandler.onRead : u)(this.typeName, message, fieldNo, wireType, d);
-            }
-        }
-        return message;
-    }
-    internalBinaryWrite(message: GetPwdUpdateTmReply, writer: IBinaryWriter, options: BinaryWriteOptions): IBinaryWriter {
-        /* bool needChangePwd = 1; */
-        if (message.needChangePwd !== false)
-            writer.tag(1, WireType.Varint).bool(message.needChangePwd);
-        /* string pwdUpdateTm = 2; */
-        if (message.pwdUpdateTm !== "")
-            writer.tag(2, WireType.LengthDelimited).string(message.pwdUpdateTm);
-        let u = options.writeUnknownFields;
-        if (u !== false)
-            (u == true ? UnknownFieldHandler.onWrite : u)(this.typeName, message, writer);
-        return writer;
-    }
-}
-/**
- * @generated MessageType for protobuf message ada.GetPwdUpdateTmReply
- */
-export const GetPwdUpdateTmReply = new GetPwdUpdateTmReply$Type();
 // @generated message type with reflection information, may provide speed optimized methods
 class ListDomainReq$Type extends MessageType<ListDomainReq> {
     constructor() {
@@ -23215,7 +23091,6 @@ export const ADA = new ServiceType("ada.ADA", [
     { name: "DisableMfa", options: {}, I: DisableMfaReq, O: DisableMfaReply },
     { name: "UpdateAvatar", options: {}, I: UpdateAvatarReq, O: UpdateAvatarReply },
     { name: "ResetPassword", options: {}, I: ResetPasswordReq, O: ResetPasswordReply },
-    { name: "GetPwdUpdateTm", options: {}, I: GetPwdUpdateTmReq, O: GetPwdUpdateTmReply },
     { name: "ListDomain", options: {}, I: ListDomainReq, O: ListDomainReply },
     { name: "AddDomain", options: {}, I: AddDomainReq, O: AddDomainReply },
     { name: "TestDomain", options: {}, I: TestDomainReq, O: TestDomainReply },
